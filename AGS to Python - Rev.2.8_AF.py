13a24c4b8d61550e95fc8edb1271e68dc8c484f5
Anton <guitarist177@gmail.com>
2022-10-13 17:50:52 +0100

    Working DETS

--- AGS to Python - Rev.2.8_AF.py
+++ AGS to Python - Rev.2.8_AF.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import filedialog, Listbox, scrolledtext, messagebox
-from contextlib import suppress
 from python_ags4 import AGS4
 from pandasgui import show
 import pandas as pd
@@ -19,7 +18,7 @@ class Application(ct.CTkFrame):
         ct.CTkFrame.__init__(self, master, corner_radius=15, fg_color="#f0f0f0")
 
         root.iconphoto(False, tk.PhotoImage(file='images/geo.png'))
-        master.geometry('375x400')
+        master.geometry('375x450')
         master.title("AGS Tool")
         
         self.button_open = ct.CTkButton(self, text="Open File...", command=self.get_ags_file_location, fg_color="#2b4768", 
@@ -41,16 +40,20 @@ class Application(ct.CTkFrame):
         self.button_count_results.pack(pady=8)
         self.button_count_results.configure(state=tk.DISABLED)
         
-        self.button_ags_checker = ct.CTkButton(self, text="Check AGS for errors", command=self.check_ags, 
+        self.button_ags_checker = ct.CTkButton(self, text="Check AGS for Errors", command=self.check_ags, 
         corner_radius=10, fg_color="#2b4768", hover_color="#6bb7dd", text_color="#FFFFFF", text_color_disabled="#999999", text_font=("Tahoma",9))
         self.button_ags_checker.pack(pady=8)
         self.button_ags_checker.configure(state=tk.DISABLED)
 
-        self.unique_id = ct.CTkButton(self, text='''Match gINT Unique ID
-to AGS from GQM Lab''', command=self.match_unique_id, 
+        self.unique_id = ct.CTkButton(self, text='''Fix AGS from GQM''', command=self.match_unique_id, 
         corner_radius=10, fg_color="#2b4768", hover_color="#6bb7dd", text_color="#FFFFFF", text_color_disabled="#999999", text_font=("Tahoma",9))
         self.unique_id.pack(pady=8)
         self.unique_id.configure(state=tk.DISABLED)
+
+        self.dets = ct.CTkButton(self, text='''Fix AGS from DETS''', command=self.match_unique_id_dets, 
+        corner_radius=10, fg_color="#2b4768", hover_color="#6bb7dd", text_color="#FFFFFF", text_color_disabled="#999999", text_font=("Tahoma",9))
+        self.dets.pack(pady=8)
+        self.dets.configure(state=tk.DISABLED)
 
         self.del_tbl = ct.CTkButton(self, text='''Delete Non-Result tables''', command=self.del_tables, 
         corner_radius=10, fg_color="#2b4768", hover_color="#6bb7dd", text_color="#FFFFFF", text_color_disabled="#999999", text_font=("Tahoma",9))
@@ -96,6 +99,7 @@ to AGS from GQM Lab''', command=self.match_unique_id,
             'LPDN',
             'LPEN',
             'LRES',
+            'ERES',
             'LTCH',
             'LVAN',
             'RELD',
@@ -118,9 +122,9 @@ to AGS from GQM Lab''', command=self.match_unique_id,
             ]
 
     def get_ags_file_location(self):
-        self._disable_buttons
+        self._disable_buttons()
 
-        app.master.geometry('375x400')
+        app.master.geometry('375x450')
         self.text.set("Open an AGS file and choose what to do...")
         if self.box == True:
             self.listbox.pack_forget()
@@ -154,10 +158,11 @@ Please select an AGS with "Open File..."''')
         self.button_count_results.configure(state=tk.NORMAL)
         self.unique_id.configure(state=tk.NORMAL)
         self.del_tbl.configure(state=tk.NORMAL)
+        self.dets.configure(state=tk.NORMAL)
 
 
     def count_lab_results(self):
-        self._disable_buttons
+        self._disable_buttons()
 
         if self.dict_fix == True:
             self.button_fix_dict.pack_forget()
@@ -262,7 +267,7 @@ Please select an AGS with "Open File..."''')
         if self.box == False:
             self.listbox = scrolledtext.ScrolledText(self, height=10, font=("Tahoma",9))
             result_list.index.name = ' '
-            app.master.geometry('430x575')
+            app.master.geometry('430x625')
             self.listbox.tag_configure('tl', justify='left')
             self.listbox.insert('end', result_list, 'tl')
             self.listbox.delete(1.0,3.0)
@@ -275,26 +280,27 @@ Please select an AGS with "Open File..."''')
 
             self.text.set("Results list ready to export.")
         else:
-            app.master.geometry('430x575')
+            app.master.geometry('430x625')
             self.listbox.pack(padx=20)
             pass
-        self._enable_buttons
+
+        self._enable_buttons()
         
     def export_results(self):
-        self._disable_buttons
+        self._disable_buttons()
         self.path_directory = filedialog.asksaveasfilename(filetypes=[('CSV Files', '*.csv')],defaultextension="*.csv")
         if not self.path_directory:
             return
         self.result_list.to_csv(self.path_directory, index=False, index_label=False, header=None)
         print(f"File saved in:  + {str(self.path_directory)}")
 
-        self._enable_buttons
+        self._enable_buttons()
 
     def start_pandasgui(self):
-        self._disable_buttons
+        self._disable_buttons()
 
         '''save ags button can be enabled once the pandasgui is created'''
-        app.master.geometry('375x400')
+        app.master.geometry('375x450')
         self.text.set("Pandas GUI is opening, please wait...")
         if self.box == True:
             self.listbox.pack_forget()
@@ -305,8 +311,7 @@ Please select an AGS with "Open File..."''')
             self.export = False
         root.update()
         
-        self.button_save_ags.configure(state=tk.NORMAL)
-        self.del_tbl.configure(state=tk.NORMAL)
+
         try:
             self.gui = show(**self.tables)
         except:
@@ -318,11 +323,11 @@ Please select an AGS with "Open File..."''')
         for table in self.result_tables:
             if table in list(self.tables):
                 self.ags_tables.append(table)
-        self._enable_buttons
+        self._enable_buttons()
         
     def check_ags(self):
-        self._disable_buttons
-        app.master.geometry('375x400')
+        self._disable_buttons()
+        app.master.geometry('375x450')
         if self.dict_fix == True:
             self.button_fix_dict.pack_forget()
             self.dict_fix = False
@@ -360,7 +365,7 @@ Please select an AGS with "Open File..."''')
                     
         except ValueError as e:
             print(f'AGS Checker ended unexpectedly: {e}')
-            app.master.geometry('375x500')
+            app.master.geometry('375x550')
             self.text.set('''Something went wrong.
 Make sure there are "key" fields in the dictionary!
 Save a new AGS file with a fixed dictionary 
@@ -391,16 +396,16 @@ by pressing "Fix DICT errors".''')
                 self.error_list.append(f"Error in line: {error['line']}, group: {error['group']}, description: {error['desc']}")
 
         if errors:
-            app.master.geometry('375x440')
+            app.master.geometry('375x490')
             self.button_export_error = ct.CTkButton(self, text="Export Error Log", command=self.export_errors, 
             corner_radius=10, fg_color="#2b4768", hover_color="#6bb7dd", text_color="#FFFFFF", text_color_disabled="#999999", text_font=("Tahoma",9))
             self.button_export_error.pack(pady=(8,8))
             self.export = True
 
-        self._enable_buttons
+        self._enable_buttons()
 
     def export_errors(self):
-        self._disable_buttons
+        self._disable_buttons()
         
         self.log_path = filedialog.asksaveasfilename(filetypes=[('Text Files', '*.txt')],defaultextension="*.txt")
         if self.log_path == '':
@@ -409,42 +414,42 @@ by pressing "Fix DICT errors".''')
             for item in self.error_list:
                 f.write("%s\n" % item)
 
-        self._enable_buttons
+        self._enable_buttons()
 
     def fix_dict(self):
         self.tables['DICT']['DICT_STAT'].replace(to_replace="REQUIRED",value="KEY", inplace=True)
-        self._disable_buttons
+        self._disable_buttons()
         self.temp_file_name = filedialog.asksaveasfilename(filetypes=[('AGS Files', '*.ags')],defaultextension="*.ags")
         if self.temp_file_name == "":
             self.dict_fix = False
             self.button_fix_dict.pack_forget()
-            app.master.geometry('375x350')
+            app.master.geometry('375x400')
             self.text.set("Open an AGS file and choose what to do...")
             root.update()
             self.check_ags()
             return
         AGS4.dataframe_to_AGS4(self.tables, self.headings, self.temp_file_name)
         print('Done.')
-        self._enable_buttons
+        self._enable_buttons()
         self.dict_fix = False
         self.button_fix_dict.pack_forget()
-        app.master.geometry('375x400')
+        app.master.geometry('375x450')
         self.text.set("Try checking the file for errors again.")
         root.update()
 
 
     def save_ags(self):
-        self._disable_buttons
+        self._disable_buttons()
         newFileName = filedialog.asksaveasfilename(filetypes=[('AGS Files', '*.ags')],defaultextension="*.ags")
         if not newFileName:
             return
         AGS4.dataframe_to_AGS4(self.tables, self.tables, newFileName)
         print('Done.')
-        self._enable_buttons
+        self._enable_buttons()
 
 
     def match_unique_id(self):
-        self._disable_buttons
+        self._disable_buttons()
 
         self.gint_location = filedialog.askopenfilename(filetypes=[('gINT Project', '*.gpj')])
         if self.gint_location == '':
@@ -555,7 +560,7 @@ by pressing "Fix DICT errors".''')
 #                         print("Unable to match sample data from gINT.")     
 #                         self.text.set('''Couldn't match sample data.
 # Did you select the correct gint?''')
-                        root.update()
+#                        root.update()
 
                 except Exception as e:
                     print(str(e))
@@ -685,7 +690,85 @@ by pressing "Fix DICT errors".''')
         root.update()
         print("Matching complete!")
 
-        self._enable_buttons
+        self._enable_buttons()
+
+    def match_unique_id_dets(self):
+        self._disable_buttons()
+
+        self.gint_location = filedialog.askopenfilename(filetypes=[('gINT Project', '*.gpj')])
+        if self.gint_location == '':
+            messagebox.showwarning(title="Gimme a gINT!", message="You didn't select a gINT file.")
+            return
+        print(f"Matching to gINT: {self.gint_location}...")
+
+        self.text.set("Matching AGS to gINT, please wait...")
+        root.update()
+
+        gint = self.gint_location
+        conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ='+gint+';')
+        query = "SELECT * FROM SPEC"
+        gint_spec = pd.read_sql(query, conn)
+
+        if not self.ags_tables == []:
+            self.ags_tables = []
+
+        for table in self.result_tables:
+            if table in list(self.tables):
+                self.ags_tables.append(table)
+
+            try:
+                try:
+                    if 'match_id' not in gint_spec:
+                        gint_spec.insert(len(list(gint_spec.columns)),'match_id','')
+                except:
+                    pass
+                
+                try:
+                    if 'match_id' not in self.tables[table]:
+                        self.tables[table].insert(len(self.tables[table].keys()),'match_id','')
+                except:
+                    pass
+
+                gint_rows = gint_spec.shape[0]
+
+                for row in range (0,gint_rows):
+                    gint_spec['match_id'][row] = str(gint_spec['PointID'][row]) + str(format(gint_spec['Depth'][row],'.2f'))
+
+                for row in range (2,len(self.tables[table])):
+                    self.tables[table]['match_id'][row] = str(self.tables[table]['LOCA_ID'][row]).rsplit(' ', 2)[0] + str(self.tables[table]['SAMP_TOP'][row])
+
+                try:
+                    for tablerow in range(2,len(self.tables[table])):
+                        for gintrow in range(0,gint_rows):
+                            if self.tables[table]['match_id'][tablerow] == gint_spec['match_id'][gintrow]:
+                                #found = True
+                                self.tables[table]['LOCA_ID'][tablerow] = gint_spec['PointID'][gintrow]
+                                self.tables[table]['SAMP_ID'][tablerow] = gint_spec['SAMP_ID'][gintrow]
+                                self.tables[table]['SAMP_REF'][tablerow] = gint_spec['SAMP_REF'][gintrow]
+                                self.tables[table]['SAMP_TYPE'][tablerow] = gint_spec['SAMP_TYPE'][gintrow]
+                                self.tables[table]['SPEC_REF'][tablerow] = gint_spec['SPEC_REF'][gintrow]
+                                self.tables[table]['SAMP_TOP'][tablerow] = format(gint_spec['SAMP_Depth'][gintrow],'.2f')
+                                self.tables[table]['SPEC_DPTH'][tablerow] = format(gint_spec['Depth'][gintrow],'.2f')
+                                
+                                for x in self.tables[table].keys():
+                                        if "LAB" in x:
+                                            self.tables[table][x][tablerow] = "DETS"
+                except:
+                    pass
+
+                self.tables[table].drop(['match_id'], axis=1, inplace=True)
+
+            except Exception as e:
+                print(f"Couldn't find table, skipping... {str(e)}")
+                pass
+
+        self.button_save_ags.configure(state=tk.NORMAL)
+        self.del_tbl.configure(state=tk.NORMAL)
+        self.text.set("Matching complete! Click: 'Save AGS file'.")
+        root.update()
+        print("Matching complete!")
+
+        self._enable_buttons()
         
     def del_tables(self):
 
@@ -715,6 +798,7 @@ by pressing "Fix DICT errors".''')
         self.button_save_ags.configure(state=tk.DISABLED)
         self.unique_id.configure(state=tk.DISABLED)
         self.del_tbl.configure(state=tk.DISABLED)
+        self.dets.configure(state=tk.DISABLED)
         
     def _enable_buttons(self):
         self.button_open.configure(state=tk.NORMAL)
@@ -724,6 +808,7 @@ by pressing "Fix DICT errors".''')
         self.button_save_ags.configure(state=tk.NORMAL)
         self.unique_id.configure(state=tk.NORMAL)
         self.del_tbl.configure(state=tk.NORMAL)
+        self.dets.configure(state=tk.NORMAL)
 
 root = ct.CTk()
 app = Application(root)
