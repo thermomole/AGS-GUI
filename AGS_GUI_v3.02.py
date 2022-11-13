45c27ca33aa63a2e2cba5a696a09c4565e3707bf
Anton <78860436+lachesis17@users.noreply.github.com>
2022-11-13 13:10:54 +0000

    Separated splash script
    Still need to handle invald command name error, making it another script and using os / subprocess pkill / exit doesn't work, neither does suppressing output

--- AGS_GUI_v3.02.py
+++ AGS_GUI_v3.02.py
@@ -9,51 +9,8 @@ import pandas as pd
 import customtkinter as ct
 import pyodbc
 import warnings
+import common.splash as splash
 warnings.filterwarnings("ignore")
-
-# add check when defining testing lab for each table to check if string is in lab
-# if not in any table flag warning for incorrect AGS instead of ERES/GCHM check for DETS
-# split match functions to smaller functions where possible
-
-splash = ct.CTk()
-
-screen_width = splash.winfo_screenwidth()
-screen_height = splash.winfo_screenheight()
-
-if screen_height == 1440:
-    wid = int(screen_width / 2.5)
-    hei = int(screen_height / 2.5)
-elif screen_height == 2160:
-    wid = int(screen_width / 2)
-    hei = int(screen_height / 2)
-else:
-    wid = int(screen_width / 3)
-    hei = int(screen_height / 3)
-
-splash.geometry(f"510x255+{wid}+{hei}")
-
-splash.wm_overrideredirect(True)
-splash.lift()
-splash.wm_attributes("-transparentcolor", "black")
-splash_logo = Image.open('images/GQ_AGS.PNG')
-splash_img = ImageTk.PhotoImage(splash_logo)
-splash_label = tk.Label(image=splash_img,bg='black')
-splash_label.image = splash_img
-splash_label.place(x=0,y=0)
-
-def splash_init():
-    global after_id
-    after_id = splash.after(4000,lambda:del_splash())
-    splash.mainloop()
-    
-def del_splash():
-    global after_id
-    splash.after_cancel(after_id)
-    after_id = None
-    splash.destroy()
-
-splash_init()
-
 
 class Application(ct.CTkFrame):
 
